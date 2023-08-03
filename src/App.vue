935ebeb0ad6fe872707f11d7caae242e6db0cb92
palashkumardas <info.palash27@gmail.com>
2023-08-03 23:15:39 +0600

    epitwo done

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,31 @@
 <script setup>
 import{ref,reactive} from 'vue';
-const colorName= ref("None");
 
-function getColor(){
-  return {
-    backgroundColor: colorName.value,
-  }
-}
+const person =reactive({
+  name:'Palash',
+  subject:'cse',
+  age:30,
+  job:'software engineer'
+})
 </script>
 
 <template>
-    <section class="mx-auto container">
+  <section class="mx-auto container">
     <h1 class="text-2xl mb-10">Vue Form</h1>
-    <p class="pb-5">Your Favorite Color:{{ colorName }}</p>
-    <div>
-      Your Favorite Color: 
-      <input class="p-5" v-model="colorName" />
-      <div class="w-32 h-32 mt-10" :style="getColor()"></div>
+    <p class="mb-10">
+      {{ person }}
+    </p>
+    
+    <!-- <div class="flex flex-col mb-5 text-left" >
+      <label for="name">Name</label>
+      <input type="text" class="border border-gray-300 rounded-md p-2" v-model="person.name" />
+    </div> -->
+
+    <div class="flex flex-col mb-5 text-left" v-for="(value,key,index) in person" :key="index">
+      <label for="name">{{ key }}</label>
+      <input type="text" class="border border-gray-300 rounded-md p-2" v-model="person[key]" />
     </div>
+
   </section>
 </template>
 
