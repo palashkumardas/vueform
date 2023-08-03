04612198e00a336ca72da1e02b107f6a0637dbfa
palashkumardas <info.palash27@gmail.com>
2023-08-03 22:10:57 +0600

    episode one

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import{ref,reactive} from 'vue';
+const colorName= ref("None");
+
+function getColor(){
+  return {
+    backgroundColor: colorName.value,
+  }
+}
+</script>
+
+<template>
+    <section class="mx-auto container">
+    <h1 class="text-2xl mb-10">Vue Form</h1>
+    <p class="pb-5">Your Favorite Color:{{ colorName }}</p>
+    <div>
+      Your Favorite Color: 
+      <input class="p-5" v-model="colorName" />
+      <div class="w-32 h-32 mt-10" :style="getColor()"></div>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+
+</style>
